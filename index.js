cbe175245c6af0bd4ba1fd2ab4875d6e18aa39b2
Andrew G. Ayad <anderw_king2010@yahoo.com>
2023-09-04 20:20:40 +0200

    added test code but needs a subs and payment method

---
+++ index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAPI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion.choices);
+}
+
+main();
